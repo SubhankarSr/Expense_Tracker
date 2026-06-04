47da276e485087c21f1591e1731e78b4a5961893
Subhankar Srivastava <subhankarsrivastava480@gmail.com>
2026-06-04 12:17:56 +0530

    Refactor updateChart to handle no data scenario
    Updated chart rendering logic to handle empty data and changed chart type to doughnut.

--- chart.js
+++ chart.js
@@ -2,19 +2,16 @@ let chart;
 
 function updateChart(){
 
-let ctx =
+const ctx =
 document.getElementById(
 "expenseChart"
 );
 
 let categoryTotals = {};
 
-
 expenses.forEach(item=>{
 
-if(
-categoryTotals[item.category]
-){
+if(categoryTotals[item.category]){
 
 categoryTotals[item.category] +=
 Number(item.amount);
@@ -30,33 +27,33 @@ Number(item.amount);
 });
 
 
-
 if(chart){
 
 chart.destroy();
 
 }
 
+if(Object.keys(
+categoryTotals
+).length===0){
 
 chart = new Chart(ctx,{
 
-type:"pie",
+type:"doughnut",
 
 data:{
 
-labels:
-Object.keys(
-categoryTotals
-),
+labels:["No Data"],
 
 datasets:[{
 
-label:"Expenses",
+data:[1],
 
-data:
-Object.values(
-categoryTotals
-)
+backgroundColor:[
+"#475569"
+],
+
+borderWidth:0
 
 }]
 
@@ -72,7 +69,105 @@ plugins:{
 
 legend:{
 
-position:"bottom"
+labels:{
+color:"white"
+}
+
+}
+
+}
+
+}
+
+});
+
+return;
+
+}
+
+chart = new Chart(ctx,{
+
+type:"doughnut",
+
+data:{
+
+labels:
+Object.keys(
+categoryTotals
+),
+
+datasets:[{
+
+label:"Expenses",
+
+data:
+Object.values(
+categoryTotals
+),
+
+backgroundColor:[
+
+"#3b82f6",
+"#06b6d4",
+"#8b5cf6",
+"#f59e0b",
+"#10b981",
+"#ef4444"
+
+],
+
+borderWidth:2,
+
+hoverOffset:20
+
+}]
+
+},
+
+options:{
+
+responsive:true,
+
+maintainAspectRatio:false,
+
+cutout:"60%",
+
+animation:{
+
+animateRotate:true,
+duration:1500
+
+},
+
+plugins:{
+
+legend:{
+
+position:"bottom",
+
+labels:{
+
+color:"white",
+
+padding:20,
+
+font:{
+
+size:13
+
+}
+
+}
+
+},
+
+tooltip:{
+
+backgroundColor:"#0f172a",
+
+titleColor:"#fff",
+
+bodyColor:"#fff"
 
 }
 
@@ -83,6 +178,5 @@ position:"bottom"
 });
 
 }
-
 
 updateChart();
